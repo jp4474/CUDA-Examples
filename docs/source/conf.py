3d0fb6b0a2cdcb17d0fdbb79468ba0d2d41493ff
Jun Won (Lakon) Park <145818516+jp4474@users.noreply.github.com>
2024-05-16 15:06:38 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'CUDA'
+copyright = '2024, RaneLab'
+author = 'Jun Won Park'
 
 release = '0.1'
 version = '0.1.0'
